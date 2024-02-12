f0609269df4103d0021ab533e4286ce9c8780137
jenishvavdiya <jenishvavadiya3125@gmail.com>
2024-02-12 21:10:50 +0530

    first commit

---
+++ src/main/java/com/example/housing/HousingApplication.java
@@ -0,0 +1,13 @@
+package com.example.housing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingApplication.class, args);
+	}
+
+}
